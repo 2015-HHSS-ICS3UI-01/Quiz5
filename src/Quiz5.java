69424dc58887e4a2c2482945c8f256133a2dd761
Sami <Sami@Macentosh>
2015-11-24 15:18:24 -0500

    First Assignment

--- src/Quiz5.java
+++ src/Quiz5.java
@@ -15,31 +15,41 @@ public class Quiz5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        //new scanner
         Scanner input = new Scanner(System.in);
-
+        
+        //gets repeated until quit! is typed
         while (true) {
             //asks user for american word to be translated
             System.out.print("Please enter a word to translate(Type quit! to end the program): ");
             String word = input.nextLine();
-
+            
+            //asks for user to type a word with less than 63 characters
+            while(word.length() > 63){
+                System.out.print("Please enter a word with less than 63 characters: ");
+                word = input.nextLine();
+            }
+            //users word is changed to american
             String american = word;
+
             //program ands if quit! is entered
             if (american.equalsIgnoreCase("quit!")) {
                 break;
             }
-            if (american.length() < 4) {
-                System.out.println(american + " in Canadian is the same!");
+            //if word doesnt contain or then it says that its the same
+            if (!american.contains("or")) {
+                System.out.println(word + " in Canadian is the same!");
             }
+            //adds our at the end of the word
             for (int i = 0; i < american.length(); i++) {
-                char o = american.charAt(i);                              
-                if (o == 'o') {
-                    if (american.contains("or")) {
-                        int oFound = american.indexOf(o);                      
-                        String first = american.substring(0, oFound);
-                        String middle = american.substring(oFound);
-                        american = first + "our";                       
-                        System.out.println(american);
+                char r = american.charAt(i);
+                if (american.endsWith("or")) {
+                    if (r == 'r') {
+                        int rFound = american.indexOf(r);
+                        String first = american.substring(0, rFound);
+                        String middle = american.substring(rFound);
+                        american = first + "ur";
+                        System.out.println(word + " translates to " + american + " in Canadian");
                         break;
                     }
                 }
