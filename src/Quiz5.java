7f6d1e62ebeb0e35e5cee6f319b89cc2d24e8757
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-24 14:14:54 -0500

    Quiz 5 done

--- src/Quiz5.java
+++ src/Quiz5.java
@@ -34,12 +34,31 @@ public class Quiz5 {
             }
 
             //translate from american to canadian if the word ends with "or" and has 4 or more letters
-            if (word.endsWith("or") && word.length() >= 4) {
+            if ((word.endsWith("or")) && (word.length() >= 4) && (word.endsWith("bor")
+                    || word.endsWith("cor")
+                    || word.endsWith("dor")
+                    || word.endsWith("for")
+                    || word.endsWith("gor")
+                    || word.endsWith("hor")
+                    || word.endsWith("jor")
+                    || word.endsWith("kor")
+                    || word.endsWith("lor")
+                    || word.endsWith("mor")
+                    || word.endsWith("nor")
+                    || word.endsWith("por")
+                    || word.endsWith("qor")
+                    || word.endsWith("ror")
+                    || word.endsWith("sor")
+                    || word.endsWith("tor")
+                    || word.endsWith("vor")
+                    || word.endsWith("wor")
+                    || word.endsWith("xor")
+                    || word.endsWith("yor")
+                    || word.endsWith("zor"))) {
                 //replace "r" with "ur"
                 canadian = word.replace("r", "ur");
                 System.out.println(word + " translates to " + canadian + " in Canadian");
-            }
-            //if the word doesnt end with "or" or isnt 4 letters or longer
+            } //if the word doesnt end with "or" or isnt 4 letters or longer
             else {
                 //tell them that their word doesnt change
                 System.out.println(word);
