165aa19eb34e66a1d80e25e8dc763f73cb451173
Hadida <Hadida@Hadida-PC>
2015-11-24 15:26:16 -0500

    done

--- src/Quiz5.java
+++ src/Quiz5.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author hadik9595
@@ -16,32 +15,36 @@ public class Quiz5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
-        while(true){
-        System.out.println("Enter an American word to translate into Canadian");
-       String word = in.nextLine();
-       
-       int numChars = word.length();
-       
-       int real = numChars - 2;
-       
-            
-       if(numChars > 5 && word.endsWith("or") ){
-           int oFound = word.indexOf("or");
-           String first = word.substring(0, oFound);
-           String american = first + "our";
-            System.out.println( word + " translates to "  +american+ " in Canadian");
-           
-          
-       }if(word.equalsIgnoreCase("quit!")){
-            System.out.println("You quit the program!");
-            break;
-       }if(numChars < 5){
-            System.out.println(word+" translates to " + word + " in Canadian");
-       }
-                   
-         
-           }
-       }
-    }
+        // generates scanner
+        Scanner in = new Scanner(System.in);
+        // a continious infinite while loop
+        while (true) {
+            // asks for words to translate and stores it as "word"
+            System.out.println("Enter an American word to translate into Canadian");
+            String word = in.nextLine();
 
+            // If user input quit, program breaks! (stops)
+            if (word.equalsIgnoreCase("quit!")) {
+                System.out.println("You quit the program!");
+                break;
+            }
+            // obtains length of word
+            int numChars = word.length();
+
+            // If word is 5 or more letter long and ends with OR
+            if (numChars >= 5 && numChars < 64 && word.endsWith("or")) {
+                // removes the or out of word
+                int withoutOR = numChars - 2;
+                //substring the whole word leaving OR behind 
+                String first = word.substring(0, withoutOR);
+                // and prints it with an ending of "our"
+                String american = first + "our";
+                System.out.println(word + " translates to " + american + " in Canadian");
+                
+            } // otherwise print out whatever the user inputs back to him
+            else {
+                System.out.println(word + " translates to " + word + " in Canadian");
+            }
+        }
+    }
+}
