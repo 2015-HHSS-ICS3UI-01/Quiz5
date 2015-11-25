17bbab8efb287279af64933d6e44e3b3783e673d
malcr1272 <malcr1272@HRH-IC0027320.wrdsb.ca>
2015-11-25 13:14:44 -0500

    quiz 5

---
+++ src/quiz5.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author malcr1272
+ */
+public class quiz5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //make scanner
+        Scanner in = new Scanner(System.in);       
+        //create string for the word
+        String word = "";
+        //Create while loop to ask which words to translate
+        while (!word.equals("quit!")) {
+            System.out.println("Enter an American word to translate into canadian");
+            word = in.nextLine();
+            
+        int length = word.length();
+        
+       String noend = word.replace("or", "");
+               
+            if (word.endsWith("or") && (length > 4) && 
+               (!noend.endsWith("a")||
+               !noend.endsWith("e")||
+               !noend.endsWith("i")||
+               !noend.endsWith("o")||
+               !noend.endsWith("u")||
+               !noend.endsWith("y"))){
+              
+              String canadian = word.replace("or", "our");
+              
+                System.out.println(word + " translates to " + canadian + " in canadian");
+            }else{
+                System.out.println(word + " translates to " + word + " in canadian");
+            }
+        }
+    }
+}
